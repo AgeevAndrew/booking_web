aaeb7ef846b0582b2daa76a7f13477d558a00430
kirillvs <kirillvs@mail.ru>
2017-12-20 11:58:35 +1000

    devise custom confirmation

---
+++ app/mailers/devise_mailer.rb
@@ -0,0 +1,11 @@
+class DeviseMailer < Devise::Mailer
+  helper :application # gives access to all helpers defined within `application_helper`.
+  include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
+  default template_path: 'devise/mailer' # to make sure that your mailer uses the devise views
+
+  def confirmation_instructions(record, token, opts={})
+    opts[:to] = JSON.parse(Figaro.env.admin_mails)
+    super
+  end
+
+end
