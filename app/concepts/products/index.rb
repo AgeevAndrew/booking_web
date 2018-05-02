49bf32e43fac3bccabc2278cc59aae5400478c64
Andrew Ageev <ageev86@gmail.com>
2018-05-02 14:37:06 +1000

    Remove deleted products

--- app/concepts/products/index.rb
+++ app/concepts/products/index.rb
@@ -15,7 +15,7 @@ class Products::Index < ApplicationOperation
   def process(params)
     company = params[:current_user].company
     @categories = Category.all
-    @products = Product.all
+    @products = Product.without_deleted
     if company
       @products = @products.where(company_id: company.id)
       @categories = @categories.where(id: company.categories)
