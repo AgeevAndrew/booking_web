d4506e42ac822fff633b59f0b4375f1c9e2d8a79
Aandrew Ageev <ageev86@gmail.com>
2018-06-07 16:17:44 +1000

    Fix time

--- app/mailers/operator_mailer.rb
+++ app/mailers/operator_mailer.rb
@@ -2,7 +2,7 @@
 
 class OperatorMailer < ApplicationMailer
   def new_order(operators, order_id)
-    @order = Order.find(order_id)
+    @order = Order.find(order_id).decorate
     @order_token = OrderToken.find_by(order_id: order_id)
     mail(to: operators, subject: t('.subject', num: @order.num))
   end
