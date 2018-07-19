ed2cd4d6c36cd8eff7531e2e2e5c9bc0806ce26a
Andrew Ageev <ageev86@gmail.com>
2018-07-19 12:48:52 +1000

    Fix bugs

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -3,6 +3,8 @@
 class ApplicationJob < ActiveJob::Base
   private
 
+  attr_reader :order
+
   def operators
     return order.company.contact_info['email'] if Rails.env.production?
     ['ageevaa@arink-group.ru', 'mokovvy@arink-group.ru']
