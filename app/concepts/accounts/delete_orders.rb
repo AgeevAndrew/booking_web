ea4c521787b74c5f6da5275d110d66c06f011b01
fixen94 <fixen94@gmail.com>
2018-08-15 16:02:29 +1000

    Исправлена операция

--- app/concepts/accounts/delete_orders.rb
+++ app/concepts/accounts/delete_orders.rb
@@ -2,8 +2,12 @@
 
 module Accounts
   class DeleteOrders < ApplicationOperation
+    def model!(params)
+      Account.where(id:params[:id]).first
+    end
+
     def process(params)
-      Account.find(params[:id]).orders.destroy_all
+      model.orders.destroy_all if model
     end
   end
 end
